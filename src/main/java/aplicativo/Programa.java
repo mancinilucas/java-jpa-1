36f813aeab3f038a915fec83ff6adac9375ca723
mancinilucas <mancini.lucasg@gmail.com>
2023-02-21 10:25:17 -0300

    find obj implementation

--- src/main/java/aplicativo/Programa.java
+++ src/main/java/aplicativo/Programa.java
@@ -8,19 +8,17 @@ import javax.persistence.Persistence;
 
 public class Programa {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(null, "Miga Gama", "miga@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Musa Gama", "musa@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Bel Gama", "bel@gmail.com");
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
-        em.getTransaction().begin(); //incia a transação com DB
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit(); //confirma as alterações no DB
+        //Buscar objeto no DB, converter para obj e instanciar obj
+        Pessoa p = em.find(Pessoa.class, 2);
+        System.out.println(p);
+
         System.out.println("Pronto!");
+        em.close();
+        emf.close();
 
     }
 }
